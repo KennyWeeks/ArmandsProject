c8492b5d6196ad1dbb4389905a4fc0a1fb4b0bb1
KennyWeeks <weeksgit@gmail.com>
2023-02-11 21:57:26 -0800

    This will just hold the changes that are made to the files so I can have a cloud copy.

---
+++ demerit/src/views/BodyParts.svelte
@@ -0,0 +1,19 @@
+<script></script>
+
+<div class="bodypart">
+
+    <slot></slot>
+</div>
+
+<style lang="scss">
+
+    .bodypart {
+        padding:10px;
+        border:2px solid #000;
+        position:absolute;
+        top:50%;
+        left:50%;
+        transform:translate(-50%, -50%);
+    }
+
+</style>
