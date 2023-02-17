53ecc2aa1d44af3381303e52c34a6d626409200b
KennyWeeks <weeksgit@gmail.com>
2023-02-17 11:21:10 -0800

    Changed the styles to inline, cause the reduces the iteration count.

--- demerit/src/views/BodyParts.svelte
+++ demerit/src/views/BodyParts.svelte
@@ -14,13 +14,15 @@
 
     var printCommand = ()=>{
         let mainContent = document.getElementById("main-component");
-        let height = 8.5 * 96;
+        /*let height = 8.5 * 96;
         let width = 11 * 96;
         var newWindow = window.open("", "PRINT", `height=${height}px, width=${width}px`);
         newWindow.document.write("<div id='main-component'></div>");
         var testing = newWindow.document.getElementById("main-component");
-        testing.style.cssText = window.getComputedStyle(mainContent).cssText;
-        var t = window.getComputedStyle(mainContent);
+        console.log(mainContent.getAttribute("style"));
+        testing.setAttribute("style", mainContent.getAttribute("style"));*/
+        console.log()
+        /*var t = window.getComputedStyle(mainContent);
         console.log(t);
         for(let key in t) {
             //console.log(key + " -> " + t[key]);
@@ -29,12 +31,12 @@
                 let string = key + ": " + h + "; box-shadow: inset 0 0 0 1px #000;";
                 testing.style.cssText = string;
             }
-        }
-        testing.innerHTML = mainContent.innerHTML;
+        }*/
+        //testing.innerHTML = mainContent.innerHTML;
         //newWindow.document.write(mainContent.innerHTML);
         //var testing = newWindow.document.getElementById("main-component");
-        console.log(testing);
-        newWindow.focus();
+        //console.log(testing);
+        //newWindow.focus();
     }
 
     onMount(async ()=>{
--- demerit/src/views/DemoView.svelte
+++ demerit/src/views/DemoView.svelte
@@ -1,64 +1,13 @@
 <script></script>
 
-<div id="main-component">
+<!--The reason I am making everything inline styled is because
+the printing process of these pages will not carry the styles over, and 
+in svelte, the .css is bundled, so I actually don't have access to it until
+the final render, so this process just makes everything easier because
+I can map the childnodes of the 'main-component' to an object, and 
+just copy the style strings over-->
 
-    <img src="header.png"/>
-
-    <div id="header">
-
-        <div class="itemblocks" id="test">
-
-            <h3 class="description">Name</h3>
-
-        </div>
-
-        <div class="itemblocks">
-
-            <h3 class="description">Date</h3>
-
-        </div>
-
-        <div class="itemblocks">
-
-            <h3 class="description">HR #</h3>
-
-        </div>
-
-    </div>
-
-    <h3 id="infraction">Infraction:</h3>
-
-    <p id="infraction-starting-line">I earned <span id="demerit-points">(#)</span> demerit points at approximately <span id="time">(time)</span> for a/an <span id="infraction-space">(infraction)</span>. <span id="assigned">(Who assigned
-    you the demerits, if Capt. Spell out Captain)</span> assigned me this demerit report because <span id="demerit-description">...</span></p>
-
-    <table cellspacing="0">
-        <colgroup>
-            <col span="1" style="width: 0%;">
-            <col span="1" style="width: 45%;">
-            <col span="1" style="width: 30%;">
-            <col span="1" style="width: 25%;">
-        </colgroup>
-        <tr>
-            <td></td>
-            <td>Recruit Signature</td>
-            <td>HR #</td>
-            <td>Date</td>
-        </tr>
-        <tr>
-            <td></td>
-            <td>Academy Staff Signature</td>
-            <td>HR #</td>
-            <td>Date</td>
-        </tr>
-    </table>
-
-</div>
-
-<style lang="scss">
-
-    #main-component {
-        /*This is gonna be the piece of paper*/
-        width:calc(8.5*96px);
+<!--width:calc(8.5*96px);
         height:calc(11*96px);
         box-shadow:inset 0 0 0 1px #000;
         position:absolute;
@@ -67,68 +16,98 @@
         transform:translate(-50%);
         margin-bottom:10px;
         overflow:hidden;
-        background-color:#00ff00;
+        background-color:#00ff00;-->
 
-        img {
-            margin-top:calc(1*96px);
+<div id="main-component" style="width:calc(8.5*96px); height:calc(11*96px); box-shadow:inset 0 0 0 1px #000; position:absolute; top:100vh;
+left:50%; transform:translate(-50%); margin-bottom:10px; overflow:hidden; background-color:#00ff00;">
+
+    <!--margin-top:calc(1*96px);
             margin-left:calc(1*96px);
-            border:2px solid #000;
-        }
+            border:2px solid #000;-->
 
-        #header {
-            width:calc(6.5*96px);
+    <img src="header.png" style="margin-top:96px; margin-left:96px; border:2px solid #000;"/>
+
+    <!--width:calc(6.5*96px);
             border-bottom:3px solid #000;
             display:flex;
             justify-content:space-between;
             flex-flow:row;
             flex-wrap:nowrap;
-            margin-left:96px;
+            margin-left:96px;-->
 
-            #test {
-                flex-grow:2;
-            }
+    <div id="header" style="width:calc(6.5*96px); border-bottom:3px solid #000; display:flex; justify-content:space-between;
+    flex-flow:row; flex-wrap:nowrap; margin-left:96px;">
 
-            .itemblocks {
-                flex-grow:1;
-            }
 
-            .itemblocks > h3 {
-                font-size:18px;
-                margin-bottom:0px;
-            }
-        }
+        <div class="itemblocks" id="test" style="font-size:18px; margin-bottom:0px; flex-grow:2;">
 
-        #infraction {
-            text-transform:uppercase;
+            <h3 class="description" style="margin-bottom:0px; font-weight:normal;">Name</h3>
+
+        </div>
+
+        <div class="itemblocks" style="font-size:18px; margin-bottom:0px; flex-grow:1;">
+
+            <h3 class="description" style="margin-bottom:0px; font-weight:normal;">Date</h3>
+
+        </div>
+
+        <div class="itemblocks" style="font-size:18px; margin-bottom:0px; flex-grow:1;">
+
+            <h3 class="description" style="margin-bottom:0px; font-weight:normal;">HR #</h3>
+
+        </div>
+
+    </div>
+
+    <!--text-transform:uppercase;
             font-weight:normal;
             margin-left:96px;
-            font-size:16px;
-        }
+            font-size:16px;-->
 
-        #infraction-starting-line {
-            width:calc(6.5*96px);
+    <h3 id="infraction" style="text-transform:uppercase; font-weight:normal; margin-left:96px; font-size:16px;">Infraction:</h3>
+
+    <!--width:calc(6.5*96px);
             margin-left:96px;
-            font-size:15.25px;
-        }
+            font-size:15.25px;-->
 
-        table {
-            width:calc(6.5*96px + 2px);
+    <p id="infraction-starting-line" style="width:calc(6.5*96px); margin-left:96px; font-size:15.25px;">I earned <span id="demerit-points">(#)</span> demerit points at approximately <span id="time">(time)</span> for a/an <span id="infraction-space">(infraction)</span>. <span id="assigned">(Who assigned
+    you the demerits, if Capt. Spell out Captain)</span> assigned me this demerit report because <span id="demerit-description">...</span></p>
+
+    <!--width:calc(6.5*96px + 2px);
             border-bottom:0.25px solid #000;
             border-right:0.25px solid #000;
             position:absolute;
             bottom:96px;
-            left:96px;
+            left:96px;-->
 
-            tr > td {
-                box-shadow:inset 0 0 0 0.25px #000;
+    <table cellspacing="0" style="width:calc(6.5*96px); border-bottom:0.25px solid #000; border-right:0.25px solid #000; position:absolute; bottom:96px; left:96px;">
+        <colgroup>
+            <col span="1" style="width: 0%;">
+            <col span="1" style="width: 45%;">
+            <col span="1" style="width: 30%;">
+            <col span="1" style="width: 25%;">
+        </colgroup>
+        <tr>
+            <!--box-shadow:inset 0 0 0 0.25px #000;
                 overflow:hidden;
                 padding:10px;
                 padding-top:5px;
                 padding-bottom:35px;
                 font-style:italic;
-                font-size:14px;
-            }
-        }
-    }
+                font-size:14px;-->
+            <td style="box-shadow:inset 0 0 0 0.25px #000; overflow:hidden; padding:10px; padding-top:5px; padding-bottom:35px; font-style:italic; font-size:14px;"></td>
+            <td style="box-shadow:inset 0 0 0 0.25px #000; overflow:hidden; padding:10px; padding-top:5px; padding-bottom:35px; font-style:italic; font-size:14px;">Recruit Signature</td>
+            <td style="box-shadow:inset 0 0 0 0.25px #000; overflow:hidden; padding:10px; padding-top:5px; padding-bottom:35px; font-style:italic; font-size:14px;">HR #</td>
+            <td style="box-shadow:inset 0 0 0 0.25px #000; overflow:hidden; padding:10px; padding-top:5px; padding-bottom:35px; font-style:italic; font-size:14px;">Date</td>
+        </tr>
+        <tr>
+            <td style="box-shadow:inset 0 0 0 0.25px #000; overflow:hidden; padding:10px; padding-top:5px; padding-bottom:35px; font-style:italic; font-size:14px;"></td>
+            <td style="box-shadow:inset 0 0 0 0.25px #000; overflow:hidden; padding:10px; padding-top:5px; padding-bottom:35px; font-style:italic; font-size:14px;">Academy Staff Signature</td>
+            <td style="box-shadow:inset 0 0 0 0.25px #000; overflow:hidden; padding:10px; padding-top:5px; padding-bottom:35px; font-style:italic; font-size:14px;">HR #</td>
+            <td style="box-shadow:inset 0 0 0 0.25px #000; overflow:hidden; padding:10px; padding-top:5px; padding-bottom:35px; font-style:italic; font-size:14px;">Date</td>
+        </tr>
+    </table>
 
-</style>
+</div>
+
+<style lang="scss"></style>
